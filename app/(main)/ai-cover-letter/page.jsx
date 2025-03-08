ee6ada061d1920850818b298a58960f477fe9978
Shubh Jain <shubhjain191@gmail.com>
2025-03-09 02:21:44 +0530

    initial commit

---
+++ app/(main)/ai-cover-letter/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AICoversLettersPage = () => {
+  return (
+    <div>AICoversLettersPage</div>
+  )
+}
+
+export default AICoversLettersPage;
--- components/header.jsx
+++ components/header.jsx
@@ -7,9 +7,10 @@ import logo from '@/public/logo.svg'
 import { Button } from './ui/button'
 import { StarsIcon, ChevronDownIcon, LayoutDashboard, FileText, PenBox, GraduationCap } from 'lucide-react'
 import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuItem } from './ui/dropdown-menu'
+import { checkUser } from '@/lib/checkUser'
 
-
-const Header = () => {
+const Header = async () => {
+    await checkUser();
     return (
         <header className='fixed top-0 w-full border-b bg-background backdrop-blur-md z-50 supports-[backdrop-filter]:bg-background/60'>
             <nav className='container mx-auto px-4 h-16 flex items-center justify-between'>
